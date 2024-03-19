0e721e0fc7963da19900452d2d9fbe7976840cf7
andrew <andrew@devx.com.au>
2024-03-19 17:35:32 +0700

    1st commit

---
+++ booking/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    if(request.method == 'GET'):
+        # return render(request, 'booking/home.html')
+        return HttpResponse("Hello, World!")
